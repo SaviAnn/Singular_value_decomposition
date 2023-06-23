09f44266914e865a52d8cfeeb4436deab711e41c
Anna Savitskaya <126320611+SaviAnn@users.noreply.github.com>
2023-06-23 20:34:49 +0300

    singularity.py

--- singularity.py
+++ singularity.py
@@ -46,3 +46,42 @@ if uploaded_file is not None:
     trunc_V = V[:top_k, :]
     trunc_img = trunc_U@trunc_sigma@trunc_V
     st.image(trunc_img, clamp=True)
+     #Теперь для цветного
+    # Разбиваем изображение на цветовые каналы
+    red, green, blue = image.split()
+
+    # Преобразуем каждый канал в массив NumPy
+    red = np.asarray(red)
+    green = np.asarray(green)
+    blue = np.asarray(blue)
+   
+    # Выполняем SVD разложение для каждого цветового канала
+    U_red, sing_vals_red, V_red = np.linalg.svd(red)
+    U_green, sing_vals_green, V_green = np.linalg.svd(green)
+    U_blue, sing_vals_blue, V_blue = np.linalg.svd(blue)
+
+
+
+    # Обрезаем матрицы U, sigma и V до top_k компонент
+    trunc_U_red = U_red[:, :top_k]
+    trunc_sigma_red = np.diag(sing_vals_red[:top_k])
+    trunc_V_red = V_red[:top_k, :]
+
+    trunc_U_green = U_green[:, :top_k]
+    trunc_sigma_green = np.diag(sing_vals_green[:top_k])
+    trunc_V_green = V_green[:top_k, :]
+
+    trunc_U_blue = U_blue[:, :top_k]
+    trunc_sigma_blue = np.diag(sing_vals_blue[:top_k])
+    trunc_V_blue = V_blue[:top_k, :]
+
+    # Восстанавливаем цветные каналы
+    trunc_red = trunc_U_red @ trunc_sigma_red @ trunc_V_red
+    trunc_green = trunc_U_green @ trunc_sigma_green @ trunc_V_green
+    trunc_blue = trunc_U_blue @ trunc_sigma_blue @ trunc_V_blue
+
+    # Объединяем цветные каналы
+    trunc_img = Image.fromarray(np.uint8(np.dstack((trunc_red, trunc_green, trunc_blue))))
+
+    # Отображаем восстановленное цветное изображение
+    st.image(trunc_img, clamp=True, use_column_width=True)
